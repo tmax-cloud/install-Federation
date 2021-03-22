27690bf9dd4c490e38272da3fa886f5737120703
CK13 Soohwan Kim <soowh91@gmail.com>
2021-03-22 13:25:41 +0900

    Fix error

--- manifest/1.2.initCN.sh
+++ manifest/1.2.initCN.sh
@@ -6,6 +6,6 @@ kubectl apply -f yaml/_install/1.federation-crd.yaml
 
 cp yaml/_template/federation-template.yaml yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/quay.io\/kubernetes-multicluster\/kubefed:${FED_VER}/'${REGISTRY}'\/kubernetes-multicluster\/kubefed:${FED_VER}/g' yaml/_install/2.federation-v${FED_VER}.yaml
-sed -i 's/${FED_NS}/'${FED_NS}'/g' yaml/_install/2.federation-v${FED_VER}.yaml
-sed -i 's/${FED_VER}/'${FED_VERSION}'/g' yaml/_install/2.federation-v${FED_VER}.yaml
+sed -i 's/${FED_NS}/'${FED_NS}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
+sed -i 's/${FED_VERSION}/'${FED_VERSION}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 kubectl apply -f yaml/_install/2.federation-v${FED_VERSION}.yaml
