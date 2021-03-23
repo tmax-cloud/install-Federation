7e809e05473b509699568752029d5282cc7352ea
CK13 Soohwan Kim <soowh91@gmail.com>
2021-03-23 14:57:55 +0900

    fix template to use cert-manager

--- manifest/1.1.initON.sh
+++ manifest/1.1.initON.sh
@@ -1,4 +1,4 @@
-curl -L https://github.com/kubernetes-sigs/kubefed/releases/download/v${FED_VERSION}/kubefedctl-${FED_VERSION}-${OS_TYPE}-${ARCH_TYPE}.tgz
+curl -LO https://github.com/kubernetes-sigs/kubefed/releases/download/v${FED_VERSION}/kubefedctl-${FED_VERSION}-${OS_TYPE}-${ARCH_TYPE}.tgz
 tar -zxvf kubefedctl-*.tgz
 chmod u+x kubefedctl
 mv kubefedctl /usr/local/bin/kubefedctl
@@ -6,7 +6,6 @@ mv kubefedctl /usr/local/bin/kubefedctl
 kubectl apply -f yaml/_install/1.federation-crd.yaml
 
 cp yaml/_template/federation-template.yaml yaml/_install/2.federation-v${FED_VERSION}.yaml
-#sed -i 's/quay.io\/kubernetes-multicluster\/kubefed:${FED_VER}/'${REGISTRY}'\/kubernetes-multicluster\/kubefed:${FED_VER}/g' yaml/_install/2.federation-v${FED_VER}.yaml
 sed -i 's/${FED_NS}/'${FED_NS}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${FED_VERSION}/'${FED_VERSION}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 kubectl apply -f yaml/_install/2.federation-v${FED_VERSION}.yaml
