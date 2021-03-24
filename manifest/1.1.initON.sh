8fc82b48427e79f494effbfef9d1735a72f5772e
CK13 Soohwan Kim <soowh91@gmail.com>
2021-03-24 12:47:10 +0900

    add certificate script

--- manifest/1.1.initON.sh
+++ manifest/1.1.initON.sh
@@ -8,4 +8,7 @@ kubectl apply -f yaml/_install/1.federation-crd.yaml
 cp yaml/_template/federation-template.yaml yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${FED_NS}/'${FED_NS}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${FED_VERSION}/'${FED_VERSION}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
+sed -i 's/${CA_CRT}/'${CA_CRT}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
+sed -i 's/${CA_KEY}/'${CA_KEY}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
+sed -i 's/${CA_BUNDLE}/'${CA_BUNDLE}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 kubectl apply -f yaml/_install/2.federation-v${FED_VERSION}.yaml
