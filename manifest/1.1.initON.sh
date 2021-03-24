0b3fa31390ff6acec0d4b0e391035f0b8d2f3c4e
CK13 Soohwan Kim <soowh91@gmail.com>
2021-03-24 12:50:10 +0900

    fix error

--- manifest/1.1.initON.sh
+++ manifest/1.1.initON.sh
@@ -2,13 +2,14 @@ curl -LO https://github.com/kubernetes-sigs/kubefed/releases/download/v${FED_VER
 tar -zxvf kubefedctl-*.tgz
 chmod u+x kubefedctl
 mv kubefedctl /usr/local/bin/kubefedctl
+rm kubefedctl-*.tgz
 
 kubectl apply -f yaml/_install/1.federation-crd.yaml
 
 cp yaml/_template/federation-template.yaml yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${FED_NS}/'${FED_NS}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${FED_VERSION}/'${FED_VERSION}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
-sed -i 's/${CA_CRT}/'${CA_CRT}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
+sed -i 's/${CA_CERT}/'${CA_CERT}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${CA_KEY}/'${CA_KEY}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${CA_BUNDLE}/'${CA_BUNDLE}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 kubectl apply -f yaml/_install/2.federation-v${FED_VERSION}.yaml
