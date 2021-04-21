d23d2a4a2434b2f8c8428c2189fe4ffa48d9703e
CK13 Soohwan Kim <soowh91@gmail.com>
2021-04-21 10:56:44 +0900

    fix error

--- manifest/1.1.install-fed-on.sh
+++ manifest/1.1.install-fed-on.sh
@@ -9,8 +9,7 @@ kubectl apply -f yaml/_install/1.federation-crd.yaml
 cp yaml/_template/federation-template.yaml yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${FED_NS}/'${FED_NS}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 sed -i 's/${FED_VERSION}/'${FED_VERSION}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
-sed -i 's/${CA_CERT}/'${CA_CERT}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
-sed -i 's/${CA_KEY}/'${CA_KEY}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
-sed -i 's/${CA_BUNDLE}/'${CA_BUNDLE}'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
-sed -i 's/\\\\/ /g' yaml/_install/2.federation-v${FED_VERSION}.yaml
+sed -i 's/${CA_CERT}/'"${CA_CERT}"'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
+sed -i 's/${CA_KEY}/'"${CA_KEY}"'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
+sed -i 's/${CA_BUNDLE}/'"${CA_BUNDLE}"'/g' yaml/_install/2.federation-v${FED_VERSION}.yaml
 kubectl apply -f yaml/_install/2.federation-v${FED_VERSION}.yaml
