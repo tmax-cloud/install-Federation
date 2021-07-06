c8b73207b6ce4ba5c6d84bad54134a71c7ff482b
soohwan kim <soowh91@gmail.com>
2021-07-06 21:10:03 +0900

    add garbage crd deletion process

--- manifest/2.delete-fed.sh
+++ manifest/2.delete-fed.sh
@@ -8,4 +8,5 @@ sed -i 's/${CA_BUNDLE}/'"${CA_BUNDLE}"'/g' yaml/federation-delete-${FED_VERSION}
 kubectl delete federatedtypeconfig -n kube-federation-system --all
 kubectl delete -f yaml/federation-delete-${FED_VERSION}.yaml
 kubectl delete -f yaml/federation-crd-${FED_VERSION}.yaml
+kubectl delete -f yaml/federation-hypercloud-crd-delete-${FED_VERSION}.yaml
 sudo rm -f /usr/local/bin/kubefedctl
