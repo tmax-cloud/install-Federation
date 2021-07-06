b19cbada089ed0a064c2bca7dc96f32d41d16bee
soohwan kim <soowh91@gmail.com>
2021-07-06 20:56:14 +0900

    fix error

--- manifest/2.delete-fed.sh
+++ manifest/2.delete-fed.sh
@@ -1,17 +1,6 @@
 source version.conf
 
-#kubectl delete federatedtypeconfig -n kube-federation-system --all
-kubectl -n ${FED_NS} delete federatedtypeconfig \
-    pods \
-    horizontalpodautoscalers.autoscaling \
-    daemonsets.apps statefulsets.apps \
-    cronjobs.batch
-kubectl -n ${FED_NS} delete crd \
-    federatedcronjobs.types.kubefed.io \
-    federateddaemonsets.types.kubefed.io \
-    federatedhorizontalpodautoscalers.types.kubefed.io \
-    federatedpods.types.kubefed.io \
-    federatedstatefulsets.types.kubefed.io
-kubectl delete -f yaml/federation-${FED_VERSION}.yaml
+kubectl delete federatedtypeconfig -n kube-federation-system --all
+kubectl delete -f yaml/federation-delete-${FED_VERSION}.yaml
 kubectl delete -f yaml/federation-crd-${FED_VERSION}.yaml
 sudo rm -f /usr/local/bin/kubefedctl
