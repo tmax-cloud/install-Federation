5ecd175c4e8f16ef2724fb2b4c55890335b30623
soohwan kim <soowh91@gmail.com>
2021-07-06 20:59:38 +0900

    fix error

--- manifest/2.delete-fed.sh
+++ manifest/2.delete-fed.sh
@@ -1,5 +1,10 @@
 source version.conf
 
+sed -i 's/${FED_NS}/'"${FED_NS}"'/g' yaml/federation-delete-${FED_VERSION}.yaml
+sed -i 's/${FED_VERSION}/'"${FED_VERSION}"'/g' yaml/federation-delete-${FED_VERSION}.yaml
+sed -i 's/${CA_CERT}/'"${CA_CERT}"'/g' yaml/federation-delete-${FED_VERSION}.yaml
+sed -i 's/${CA_KEY}/'"${CA_KEY}"'/g' yaml/federation-delete-${FED_VERSION}.yaml
+sed -i 's/${CA_BUNDLE}/'"${CA_BUNDLE}"'/g' yaml/federation-delete-${FED_VERSION}.yaml
 kubectl delete federatedtypeconfig -n kube-federation-system --all
 kubectl delete -f yaml/federation-delete-${FED_VERSION}.yaml
 kubectl delete -f yaml/federation-crd-${FED_VERSION}.yaml
