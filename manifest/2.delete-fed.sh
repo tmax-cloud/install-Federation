05bcffc4180cb4c5715703f2fc16ab1b2e5e7ae7
sisilia91 <soowh91@gmail.com>
2021-07-14 22:36:33 -0400

    reorganization main branch

--- manifest/2.delete-fed.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete -f yaml/_install/2.federation-${FED_VERSION}.yaml
-kubectl delete -f yaml/_install/1.federation-crd.yaml
-sudo rm -f /usr/local/bin/kubefedctl \
-    yaml/_install/2.federation-${FED_VERSION}.yaml
