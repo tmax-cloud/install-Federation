55346dce69c36fecc5fa74d052fdbde8cc131cd9
soohwan kim <soowh91@gmail.com>
2021-09-23 13:25:15 +0900

    [ims][270995] federatedclusterrolebinding 생성 추가

--- manifest/1.3.install-hypercloud-crd.sh
+++ manifest/1.3.install-hypercloud-crd.sh
@@ -1,4 +1,4 @@
-HYPERCLOUD_CRD="po hpa ds sts cj" 
+HYPERCLOUD_CRD="po hpa ds sts cj clusterrolebinding" 
 for crd in ${HYPERCLOUD_CRD}; do
     kubefedctl enable ${crd} --federated-group types.kubefed.io
 done
