7bb3806a0bbbc21c08f64f2cf8225c1fef04dd3c
SooHwan Kim <soowh91@gmail.com>
2021-11-08 02:44:28 -0500

    FED NS, CERT 고정값으로 변경

--- manifest/1.1.install-fed-on.sh
+++ manifest/1.1.install-fed-on.sh
@@ -5,9 +5,5 @@ sudo cp bin/kubefedctl /usr/local/bin/kubefedctl
 
 kubectl apply -f yaml/federation-crd-${FED_VERSION}.yaml
 
-sed -i 's/${FED_NS}/'"${FED_NS}"'/g' yaml/federation-${FED_VERSION}.yaml
 sed -i 's/${FED_VERSION}/'"${FED_VERSION}"'/g' yaml/federation-${FED_VERSION}.yaml
-sed -i 's/${CA_CERT}/'"${CA_CERT}"'/g' yaml/federation-${FED_VERSION}.yaml
-sed -i 's/${CA_KEY}/'"${CA_KEY}"'/g' yaml/federation-${FED_VERSION}.yaml
-sed -i 's/${CA_BUNDLE}/'"${CA_BUNDLE}"'/g' yaml/federation-${FED_VERSION}.yaml
 kubectl apply -f yaml/federation-${FED_VERSION}.yaml
--- manifest/1.2.install-fed-cn.sh
+++ manifest/1.2.install-fed-cn.sh
@@ -10,9 +10,5 @@ sudo docker push ${REGISTRY}/kubernetes-multicluster/kubefed:${FED_VERSION}
 kubectl apply -f yaml/federation-crd-${FED_VERSION}.yaml
 
 sed -i 's/quay.io\/kubernetes-multicluster\/kubefed:${FED_VERSION}/'"${REGISTRY}"'\/kubernetes-multicluster\/kubefed:${FED_VERSION}/g' yaml/federation-${FED_VERSION}.yaml
-sed -i 's/${FED_NS}/'"${FED_NS}"'/g' yaml/federation-${FED_VERSION}.yaml
 sed -i 's/${FED_VERSION}/'"${FED_VERSION}"'/g' yaml/federation-${FED_VERSION}.yaml
-sed -i 's/${CA_CERT}/'"${CA_CERT}"'/g' yaml/federation-${FED_VERSION}.yaml
-sed -i 's/${CA_KEY}/'"${CA_KEY}"'/g' yaml/federation-${FED_VERSION}.yaml
-sed -i 's/${CA_BUNDLE}/'"${CA_BUNDLE}"'/g' yaml/federation-${FED_VERSION}.yaml
 kubectl apply -f yaml/federation-${FED_VERSION}.yaml
